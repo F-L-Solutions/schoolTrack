6cdbe665a602721800e1fc4df4995006ae9da377
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-04 17:13:14 +0200

    Add Reservation repository and services

--- src/main/java/com/FLsolutions/schoolTrack/services/EventServiceImpl.java
+++ src/main/java/com/FLsolutions/schoolTrack/services/EventServiceImpl.java
@@ -1,14 +1,15 @@
 package com.FLsolutions.schoolTrack.services;
+
 import com.FLsolutions.schoolTrack.repositories.EventRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EventServiceImpl implements EventService{
-	
+public class EventServiceImpl implements EventService {
+
 	private EventRepository eventRepository;
-	
+
 	public EventServiceImpl(EventRepository eventRepository) {
-		this.eventRepository= eventRepository;
+		this.eventRepository = eventRepository;
 	}
-	
+
 }
