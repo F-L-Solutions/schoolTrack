ab5ca3e41efe1484e160b632c689c9200a130c68
LuciePrejzova <lucie.prejzova@gmail.com>
2024-05-04 20:06:21 +0200

    Created Parent model with relationship to Kid

--- src/main/java/com/FLsolutions/schoolTrack/models/Kid.java
+++ src/main/java/com/FLsolutions/schoolTrack/models/Kid.java
@@ -7,37 +7,50 @@ import java.util.List;
 @Table(name = "kids")
 public class Kid extends User {
 
-//	private List<Parent> parents;
+	@ManyToMany(mappedBy = "kids")
+	private List<Parent> parents;
+	
 	@OneToMany(mappedBy = "kid", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
 	private List<Reservation> reservations;
 	
 //	private List<SubstituteCredit> substitutes;
 //	private List<Attendace> attendanceList;
+	
 	private DayType dayType;
 
 	public Kid() {
 		super();
 	}
 
-	public Kid(String firstName, String lastName, DayType dayType) {
+	public Kid(String firstName, String lastName, DayType dayType, List<Parent> parents) {
 		super(firstName, lastName);
 		this.dayType = dayType;
+		this.parents = parents;
 	}
 
 	public DayType getDayType() {
 		return dayType;
 	}
-	
-    public List<Reservation> getReservations() {
-        return reservations;
-    }
 
 	public void setDayType(DayType dayType) {
 		this.dayType = dayType;
 	}
 	
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+	
     public void setReservations(List<Reservation> reservations) {
         this.reservations = reservations;
     }
+
+	public List<Parent> getParents() {
+		return parents;
+	}
+
+	public void setParents(List<Parent> parents) {
+		this.parents = parents;
+	}
+    
 
 }
---
+++ src/main/java/com/FLsolutions/schoolTrack/models/Parent.java
@@ -0,0 +1,35 @@
+package com.FLsolutions.schoolTrack.models;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name= "parents")
+public class Parent extends User{
+	
+	@ManyToMany
+    @JoinTable(
+        name = "parent_kid",
+        joinColumns = @JoinColumn(name = "parent_id"),
+        inverseJoinColumns = @JoinColumn(name = "kid_id")
+    )
+    private List<Kid> kids;
+
+	public Parent() {}
+	
+	public Parent(List<Kid> kids) {
+		super();
+		this.kids = kids;
+	}
+
+	public List<Kid> getKids() {
+		return kids;
+	}
+
+	public void setKids(List<Kid> kids) {
+		this.kids = kids;
+	}
+	
+}
