c6741d7b42c5587737532997ef5563cb71c8c8db
LuciePrejzova <lucie.prejzova@gmail.com>
2024-05-04 20:27:51 +0200

    Created AdminRepository

---
+++ src/main/java/com/FLsolutions/schoolTrack/repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.FLsolutions.schoolTrack.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.FLsolutions.schoolTrack.models.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long>{
+
+}
