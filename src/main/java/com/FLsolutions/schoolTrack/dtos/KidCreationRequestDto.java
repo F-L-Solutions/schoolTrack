1260736cec9aef5560c916a333b156e16423beda
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-18 00:09:36 +0200

    Add DTO for Kid creation request

---
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/KidCreationRequestDto.java
@@ -0,0 +1,9 @@
+package com.FLsolutions.schoolTrack.dtos;
+
+public class KidCreationRequestDto extends UserCreationRequestDto{
+
+	public KidCreationRequestDto(String firstName, String lastName) {
+		super(firstName, lastName);
+	}
+
+}
