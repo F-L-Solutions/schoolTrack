fb0a7036537b66fdbac2ba30ea882766129c7a46
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-18 00:14:49 +0200

    Add Parent controller with method to handle POST request for parent creation

---
+++ src/main/java/com/FLsolutions/schoolTrack/controllers/ParentController.java
@@ -0,0 +1,29 @@
+package com.FLsolutions.schoolTrack.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.FLsolutions.schoolTrack.dtos.ParentCreationRequestDto;
+import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
+import com.FLsolutions.schoolTrack.services.ParentService;
+
+@RestController
+@RequestMapping("/parents")
+public class ParentController {
+	
+	private final ParentService parentService;
+	
+	public ParentController(ParentService parentService) {
+		this.parentService= parentService;
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<StatusResponseDto> create(ParentCreationRequestDto dto){
+		StatusResponseDto responseDto = parentService.createParent(dto);
+		return new ResponseEntity<>(responseDto, HttpStatus.OK);
+	}
+
+}
