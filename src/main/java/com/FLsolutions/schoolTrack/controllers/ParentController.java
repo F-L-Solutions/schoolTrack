ea2fe60d56b8a0c54ef91aa845e000a23ad2ef2d
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-18 19:00:30 +0200

    Add validation annotations to the controller

--- src/main/java/com/FLsolutions/schoolTrack/controllers/ParentController.java
+++ src/main/java/com/FLsolutions/schoolTrack/controllers/ParentController.java
@@ -2,6 +2,7 @@ package com.FLsolutions.schoolTrack.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +12,21 @@ import com.FLsolutions.schoolTrack.dtos.ParentCreationRequestDto;
 import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
 import com.FLsolutions.schoolTrack.services.ParentService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/parents")
+@Validated
 public class ParentController {
-	
+
 	private final ParentService parentService;
-	
+
 	public ParentController(ParentService parentService) {
-		this.parentService= parentService;
+		this.parentService = parentService;
 	}
-	
+
 	@PostMapping("")
-	public ResponseEntity<StatusResponseDto> create(@RequestBody ParentCreationRequestDto dto){
+	public ResponseEntity<StatusResponseDto> create(@Valid @RequestBody ParentCreationRequestDto dto) {
 		StatusResponseDto responseDto = parentService.createParent(dto);
 		return new ResponseEntity<>(responseDto, HttpStatus.OK);
 	}
