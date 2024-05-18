d46815b3301246c20d443c94c940225449437551
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-18 18:59:35 +0200

    Modifies Parent/UserCreationRequestDto to correctly collect data from user and add getters and setters

--- src/main/java/com/FLsolutions/schoolTrack/dtos/ParentCreationRequestDto.java
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/ParentCreationRequestDto.java
@@ -2,17 +2,53 @@ package com.FLsolutions.schoolTrack.dtos;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class ParentCreationRequestDto extends UserCreationRequestDto {
 
 	private List<KidCreationRequestDto> kids;
-	
+
+	// @NotBlank(message = "Telephone number is mandatory")
+	private String telNumber;
+
+	@Email(message = "Email should be valid")
+	@NotBlank(message = "Email is mandatory")
+	private String email;
+
 	public ParentCreationRequestDto() {
 		super();
 	}
 
-	public ParentCreationRequestDto(String firstName, String lastName, String email, String telNumber) {
-		super(firstName, lastName, email, telNumber);
+	public ParentCreationRequestDto(String firstName, String lastName, String telNumber, String email) {
+		super(firstName, lastName);
+		this.telNumber = telNumber;
+		this.email = email;
 		// this.kids= kids;
+	}
+
+	public List<KidCreationRequestDto> getKids() {
+		return kids;
+	}
+
+	public void setKids(List<KidCreationRequestDto> kids) {
+		this.kids = kids;
+	}
+
+	public String getTelNumber() {
+		return telNumber;
+	}
+
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
--- src/main/java/com/FLsolutions/schoolTrack/dtos/UserCreationRequestDto.java
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/UserCreationRequestDto.java
@@ -1,31 +1,39 @@
 package com.FLsolutions.schoolTrack.dtos;
 
-public class UserCreationRequestDto implements RequestDto{
-	
-	public final String firstName;
-	public final String lastName;
-	public final String telNumber;
-	public final String email;
-	
-	public UserCreationRequestDto() {
-		this.firstName = "";
-		this.lastName = "";
-		this.telNumber = "";
-		this.email = "";
-	}
-	
-    public UserCreationRequestDto(String firstName, String lastName, String email, String telNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.telNumber = telNumber;
-    }
-    
-    public UserCreationRequestDto(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-		this.telNumber = "";
-		this.email = "";
-    }
+import jakarta.validation.constraints.NotBlank;
 
+public class UserCreationRequestDto implements RequestDto {
+
+	@NotBlank(message = "First name is mandatory")
+	private String firstName;
+
+	@NotBlank(message = "Last name is mandatory")
+	private String lastName;
+
+
+	public UserCreationRequestDto() {
+        this.firstName = "";
+        this.lastName = "";
+	}
+
+	public UserCreationRequestDto(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
