9a1f3bc4bef87c570f0f7dc76a37ee4a6b64156f
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-18 18:56:55 +0200

    Modified ErrorResponseDto to contain more detailed information

--- src/main/java/com/FLsolutions/schoolTrack/dtos/ErrorResponseDto.java
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/ErrorResponseDto.java
@@ -1,11 +1,52 @@
 package com.FLsolutions.schoolTrack.dtos;
 
-public class ErrorResponseDto implements ResponseDto {
+import java.time.LocalDateTime;
+import java.util.List;
 
-	public final String error;
+public class ErrorResponseDto {
 
-	public ErrorResponseDto(String error) {
-		this.error = error;
+	private String message;
+	private List<String> details;
+	private LocalDateTime timestamp;
+	private int status;
+
+	public ErrorResponseDto(String message, List<String> details, int status) {
+		this.message = message;
+		this.details = details;
+		this.timestamp = LocalDateTime.now();
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public List<String> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
 	}
 
 }
