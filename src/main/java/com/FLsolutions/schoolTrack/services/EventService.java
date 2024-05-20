dddd6039875cf14a1ae8a8ac83fa32701291ff6b
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-20 21:30:01 +0200

    Created base functionality for bulk creation of events

--- src/main/java/com/FLsolutions/schoolTrack/services/EventService.java
+++ src/main/java/com/FLsolutions/schoolTrack/services/EventService.java
@@ -1,5 +1,10 @@
 package com.FLsolutions.schoolTrack.services;
 
+import com.FLsolutions.schoolTrack.dtos.EventCreationRequestDto;
+import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
+
 public interface EventService {
+	
+	StatusResponseDto bulkCreateEvents(EventCreationRequestDto dto);
 
 }
--- src/main/java/com/FLsolutions/schoolTrack/services/EventServiceImpl.java
+++ src/main/java/com/FLsolutions/schoolTrack/services/EventServiceImpl.java
@@ -1,6 +1,15 @@
 package com.FLsolutions.schoolTrack.services;
 
+import com.FLsolutions.schoolTrack.dtos.EventCreationRequestDto;
+import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
+import com.FLsolutions.schoolTrack.models.Event;
 import com.FLsolutions.schoolTrack.repositories.EventRepository;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +19,30 @@ public class EventServiceImpl implements EventService {
 
 	public EventServiceImpl(EventRepository eventRepository) {
 		this.eventRepository = eventRepository;
+	}
+	
+	public StatusResponseDto bulkCreateEvents(EventCreationRequestDto request) {
+		StatusResponseDto responseDto = new StatusResponseDto("");
+		List<Event> events = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now();
+        
+        for (int i = 0; i < request.getNumberOfEvents(); i++) {
+            // Skip weekends
+            while (currentDate.getDayOfWeek() == DayOfWeek.SATURDAY || currentDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                currentDate = currentDate.plusDays(1);
+            }
+
+            Event event = new Event(currentDate, request.getAvailableSpots(), request.getDayType());
+            events.add(event);
+            currentDate = currentDate.plusDays(1);
+        }
+
+        eventRepository.saveAll(events);
+        String lastDay = currentDate.toString();
+        
+        responseDto.setStatus(request.getNumberOfEvents() + " number of days were created. The last created event is at " + lastDay);
+        
+        return responseDto;
 	}
 
 }
