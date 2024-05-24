435ca6d0b962e601089eccedc5a00cce29d5a320
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-05-24 17:56:04 +0200

    Reworked bulkCreateEvents and created checkExistingEvents for validation against existing events

--- src/main/java/com/FLsolutions/schoolTrack/repositories/EventRepository.java
+++ src/main/java/com/FLsolutions/schoolTrack/repositories/EventRepository.java
@@ -1,10 +1,18 @@
 package com.FLsolutions.schoolTrack.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.FLsolutions.schoolTrack.models.Event;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+	
+	Optional<Event> findByDate(LocalDate date);
+	
+	List<Event> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 
 }
--- src/main/java/com/FLsolutions/schoolTrack/services/EventServiceImpl.java
+++ src/main/java/com/FLsolutions/schoolTrack/services/EventServiceImpl.java
@@ -2,6 +2,8 @@ package com.FLsolutions.schoolTrack.services;
 
 import com.FLsolutions.schoolTrack.dtos.EventCreationRequestDto;
 import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
+import com.FLsolutions.schoolTrack.exceptions.DuplicateEventException;
+import com.FLsolutions.schoolTrack.exceptions.GenericEventException;
 import com.FLsolutions.schoolTrack.models.Event;
 import com.FLsolutions.schoolTrack.repositories.EventRepository;
 
@@ -9,6 +11,8 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -20,26 +24,56 @@ public class EventServiceImpl implements EventService {
 	public EventServiceImpl(EventRepository eventRepository) {
 		this.eventRepository = eventRepository;
 	}
-	
+
 	public StatusResponseDto bulkCreateEvents(EventCreationRequestDto request) {
 		StatusResponseDto responseDto = new StatusResponseDto("");
+
 		List<Event> events = new ArrayList<>();
-        LocalDate currentDate = request.getStartDate();
-        LocalDate generationEndDate = request.getEndDate();
-        
-        while(!currentDate.isAfter(generationEndDate)) {
-        	if(currentDate.getDayOfWeek() != DayOfWeek.SATURDAY && currentDate.getDayOfWeek() != DayOfWeek.SUNDAY) {
-                Event event = new Event(currentDate, request.getAvailableSpots(), request.getDayType());
-                events.add(event);
-        	}
-        	currentDate = currentDate.plusDays(1);
-        }
-        
-        eventRepository.saveAll(events);
-        
-        responseDto.setStatus(events.size() + " number of days were created. The last created event is at " + generationEndDate.toString());
-        
-        return responseDto;
+		LocalDate generationStartDate = request.getStartDate();
+		LocalDate generationEndDate = request.getEndDate();
+		LocalDate currentDate = generationStartDate;
+		List<LocalDate> conflictingDates = checkExistingEvents(generationStartDate, generationEndDate);
+
+		if (!conflictingDates.isEmpty()) {
+			throw new DuplicateEventException(conflictingDates);
+		} else {
+			while (!currentDate.isAfter(generationEndDate)) {
+				if (currentDate.getDayOfWeek() != DayOfWeek.SATURDAY
+						&& currentDate.getDayOfWeek() != DayOfWeek.SUNDAY) {
+					Event event = new Event(currentDate, request.getAvailableSpots(), request.getDayType());
+					events.add(event);
+				}
+				currentDate = currentDate.plusDays(1);
+			}
+		}
+
+		eventRepository.saveAll(events);
+
+		responseDto.setStatus(events.size() + " number of days were created. The last created event is at "
+				+ generationEndDate.toString());
+
+		return responseDto;
 	}
 
+	private List<LocalDate> checkExistingEvents(LocalDate generationStartDate, LocalDate generationEndDate) {
+		LocalDate currentDate = generationStartDate;
+		List<Event> existingEvents = eventRepository.findAllByDateBetween(generationStartDate, generationEndDate);
+		Set<LocalDate> existingDates = existingEvents.stream().map(Event::getDate).collect(Collectors.toSet());
+		List<LocalDate> conflictingDates = new ArrayList<>();
+
+		if (generationStartDate.isAfter(generationEndDate)) {
+			throw new GenericEventException("0 number of days were created. Start date cannot be after end date");
+		} else {
+			while (!currentDate.isAfter(generationEndDate)) {
+				if (currentDate.getDayOfWeek() != DayOfWeek.SATURDAY
+						&& currentDate.getDayOfWeek() != DayOfWeek.SUNDAY) {
+					if (existingDates.contains(currentDate)) {
+						conflictingDates.add(currentDate);
+					}
+				}
+				currentDate = currentDate.plusDays(1);
+			}
+		}
+		return conflictingDates;
+	}
 }
