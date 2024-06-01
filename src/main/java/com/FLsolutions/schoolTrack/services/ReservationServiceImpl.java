656b21bddccde3213cd9b827eb42be097e004b8b
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-06-01 22:16:38 +0200

    Added validation logic for reservation creation

--- src/main/java/com/FLsolutions/schoolTrack/services/ReservationServiceImpl.java
+++ src/main/java/com/FLsolutions/schoolTrack/services/ReservationServiceImpl.java
@@ -33,29 +33,28 @@ public class ReservationServiceImpl implements ReservationService {
 
 	public StatusResponseDto createReservation(ReservationCreationRequestDto request) {
 		StatusResponseDto response = new StatusResponseDto("");
-		System.out.println("36");
-		
+
 		Kid kid = kidRepository.findByUserName(request.getKidUserName())
 				.orElseThrow(() -> new KidNotFoundException("Selected kid username was not found in the database",
 						HttpStatus.NOT_FOUND));
-		System.out.println("41");
+
 		Optional<Reservation> existingReservation = reservationRepository.findByKidIdAndDate(kid.getSysId(),
 				request.getDate());
-		System.out.println("44");
+
 		if (existingReservation.isPresent()) {
 			throw new DuplicateReservationException("Selected kid already has a reservation for this day",
 					HttpStatus.CONFLICT);
 		}
-		System.out.println("49");
+
 		Event existingEvent = eventRepository.findByDate(request.getDate())
 				.orElseThrow(() -> new GenericEventException("For selected date there was no event found in database",
 						HttpStatus.NOT_FOUND));
-		System.out.println("53");
+
 		if (existingEvent.getAvailableSpots() != 0) {
 			throw new GenericEventException("There are available spots for the day, reservation was not created",
 					HttpStatus.CONFLICT);
 		}
-		System.out.println("58");
+
 		Reservation reservation = new Reservation(request.getDate(), request.getDayType(), kid);
 		reservationRepository.save(reservation);
 		response.setStatus("Reservation for " + request.getKidUserName() + " was created.");
