0e9cee49ceac60d7ef814950ba9c9caacdb3671c
LuciePrejzova <lucie.prejzova@gmail.com>
2024-06-03 09:50:03 +0200

    Added parentSysId field

--- src/main/java/com/FLsolutions/schoolTrack/dtos/KidCreationRequestDto.java
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/KidCreationRequestDto.java
@@ -1,13 +1,27 @@
 package com.FLsolutions.schoolTrack.dtos;
 
 public class KidCreationRequestDto extends UserCreationRequestDto{
+	
+	private Long parentSysId;
 
-	public KidCreationRequestDto(String firstName, String lastName) {
+	public KidCreationRequestDto(String firstName, String lastName, Long parentSysId) {
 		super(firstName, lastName);
+		this.parentSysId = parentSysId;
+		
 	}
 	
 	public KidCreationRequestDto() {
 		super();
 	}
+
+	public Long getParentSysId() {
+		return parentSysId;
+	}
+
+	public void setParentSysId(Long parentSysId) {
+		this.parentSysId = parentSysId;
+	}
+	
+	
 
 }
