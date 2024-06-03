ff2fdece65a042bd0f6e8c3a4dd9409579f724a1
LuciePrejzova <lucie.prejzova@gmail.com>
2024-06-03 09:37:00 +0200

    Fixed ParentResponseDto

--- src/main/java/com/FLsolutions/schoolTrack/dtos/ParentResponseDto.java
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/ParentResponseDto.java
@@ -1,5 +1,6 @@
 package com.FLsolutions.schoolTrack.dtos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.FLsolutions.schoolTrack.models.Kid;
@@ -10,13 +11,22 @@ public class ParentResponseDto implements ResponseDto {
 	private final Long sysId;
 	private final String firstName;
 	private final String lastName;
-	private final List<Kid> kids;
+	private final List<String> kids;
 	
 	public ParentResponseDto(Parent parent) {
 		this.sysId = parent.getSysId();
 		this.firstName = parent.getFirstName();
 		this.lastName = parent.getLastName();
-		this.kids = parent.getKids();
+		this.kids = getKidsNames(parent.getKids());
+	}
+	
+	public List<String> getKidsNames(List<Kid> kids){
+		List<String> kidsNames = new ArrayList<String>();		
+		kids.forEach(kid -> {
+			String fullName = kid.getFirstName() + " " + kid.getLastName();
+			kidsNames.add(fullName);
+		});
+		return kidsNames;
 	}
 
 	public Long getSysId() {
@@ -31,7 +41,7 @@ public class ParentResponseDto implements ResponseDto {
 		return lastName;
 	}
 
-	public List<Kid> getKids() {
+	public List<String> getKids() {
 		return kids;
 	}
 	
