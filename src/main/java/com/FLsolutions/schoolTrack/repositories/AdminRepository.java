c71527fc3b9221bbbab121a3aa08cccb67ff3503
LuciePrejzova <lucie.prejzova@gmail.com>
2024-06-03 18:46:20 +0200

    create admin finished

--- src/main/java/com/FLsolutions/schoolTrack/repositories/AdminRepository.java
+++ src/main/java/com/FLsolutions/schoolTrack/repositories/AdminRepository.java
@@ -1,9 +1,18 @@
 package com.FLsolutions.schoolTrack.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.FLsolutions.schoolTrack.models.Admin;
+import java.util.List;
+
 
 public interface AdminRepository extends JpaRepository<Admin, Long>{
+	
+	Optional<Admin> findByEmail(String email);
+	
+	Optional<Admin> findByFirstNameAndLastName(String firstName, String lastName);
+	
 
 }
