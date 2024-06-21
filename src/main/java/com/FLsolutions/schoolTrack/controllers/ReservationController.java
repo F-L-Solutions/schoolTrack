d703b77dcdb5775116503c4658e1ae288e4cfc26
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-06-21 23:14:50 +0200

    Created new endpoint for getting reservation by id

--- src/main/java/com/FLsolutions/schoolTrack/controllers/ReservationController.java
+++ src/main/java/com/FLsolutions/schoolTrack/controllers/ReservationController.java
@@ -3,11 +3,14 @@ package com.FLsolutions.schoolTrack.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.FLsolutions.schoolTrack.dtos.ReservationCreationRequestDto;
+import com.FLsolutions.schoolTrack.dtos.ReservationResponseDto;
 import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
 import com.FLsolutions.schoolTrack.services.ReservationService;
 
@@ -25,6 +28,12 @@ public class ReservationController {
 	public ResponseEntity<StatusResponseDto> create(@RequestBody ReservationCreationRequestDto dto) {
 		StatusResponseDto response = reservationService.createReservation(dto);
 		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ReservationResponseDto> getReservationBySysId(@PathVariable("id") Long id){
+		ReservationResponseDto response = reservationService.fetchReservationBySysId(id);
+		return new ResponseEntity<ReservationResponseDto>(response, HttpStatus.OK);
 	}
 
 }
