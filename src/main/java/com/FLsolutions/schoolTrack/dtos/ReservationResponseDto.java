00130679e4b015752e13d3bbc47cb5d16ae0a91c
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-06-21 22:57:30 +0200

    Created response dto for reservation

---
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/ReservationResponseDto.java
@@ -0,0 +1,46 @@
+package com.FLsolutions.schoolTrack.dtos;
+
+import java.time.LocalDate;
+
+import com.FLsolutions.schoolTrack.models.DayType;
+import com.FLsolutions.schoolTrack.models.Kid;
+import com.FLsolutions.schoolTrack.models.Reservation;
+import com.FLsolutions.schoolTrack.models.ReservationStatus;
+
+public class ReservationResponseDto implements ResponseDto {
+
+	private final Long sysId;
+	private final LocalDate date;
+	private final DayType dayType;
+	private final Kid kid;
+	private final ReservationStatus status;
+
+	public ReservationResponseDto(Reservation reservation) {
+		this.sysId = reservation.getSysId();
+		this.date = reservation.getDate();
+		this.dayType = reservation.getDayType();
+		this.kid = reservation.getKid();
+		this.status = reservation.getStatus();
+	}
+
+	public Long getSysId() {
+		return sysId;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public DayType getDayType() {
+		return dayType;
+	}
+
+	public Kid getKid() {
+		return kid;
+	}
+
+	public ReservationStatus getStatus() {
+		return status;
+	}
+
+}
