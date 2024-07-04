536ab05afc443a46be5175839a332cc76871ee0f
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-07-04 20:25:18 +0200

    Added validation for firstName and lastName, cannot be longer than 20 chars and cannot have special chars

--- src/main/java/com/FLsolutions/schoolTrack/dtos/UserCreationRequestDto.java
+++ src/main/java/com/FLsolutions/schoolTrack/dtos/UserCreationRequestDto.java
@@ -1,15 +1,20 @@
 package com.FLsolutions.schoolTrack.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class UserCreationRequestDto implements RequestDto {
 
+	@Size(max = 20, message = "First name can be at most 20 characters long")
+	@Pattern(regexp = "^[a-zA-ZáčďéěíňóřšťúůýžÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ]*$", message = "First name should not contain special characters")
 	@NotBlank(message = "First name is mandatory")
 	private String firstName;
 
+	@Size(max = 20, message = "Last name can be at most 20 characters long")
+	@Pattern(regexp = "^[a-zA-ZáčďéěíňóřšťúůýžÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ]*$", message = "Last name should not contain special characters")
 	@NotBlank(message = "Last name is mandatory")
 	private String lastName;
-
 
 	public UserCreationRequestDto() {
         this.firstName = "";
