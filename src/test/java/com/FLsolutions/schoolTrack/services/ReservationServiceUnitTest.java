cfce9bc18c8ceb79abde1fab1187fb6f1669a95e
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-07-22 16:52:42 +0200

    Added several tests for failure scenarios of ReservationService functions

--- src/test/java/com/FLsolutions/schoolTrack/services/ReservationServiceUnitTest.java
+++ src/test/java/com/FLsolutions/schoolTrack/services/ReservationServiceUnitTest.java
@@ -1,7 +1,9 @@
 package com.FLsolutions.schoolTrack.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -21,10 +23,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import com.FLsolutions.schoolTrack.dtos.ReservationCreationRequestDto;
 import com.FLsolutions.schoolTrack.dtos.ReservationResponseDto;
 import com.FLsolutions.schoolTrack.dtos.StatusResponseDto;
+import com.FLsolutions.schoolTrack.exceptions.DuplicateReservationException;
+import com.FLsolutions.schoolTrack.exceptions.GenericEventException;
+import com.FLsolutions.schoolTrack.exceptions.GenericReservationException;
 import com.FLsolutions.schoolTrack.models.DayType;
 import com.FLsolutions.schoolTrack.models.Event;
 import com.FLsolutions.schoolTrack.models.Kid;
@@ -54,6 +60,7 @@ public class ReservationServiceUnitTest {
 	private ReservationCreationRequestDto creationRequestDto;
 	private Kid mockKid;
 	private Event mockEvent;
+	private Event mockEvent2;
 
 	@BeforeEach
 	void setUp() {
@@ -67,6 +74,11 @@ public class ReservationServiceUnitTest {
 		mockEvent.setSysId(1L);
 		mockEvent.setDate(LocalDate.of(2024, 7, 22));
 		mockEvent.setAvailableSpots(0);
+
+		mockEvent2 = new Event();
+		mockEvent2.setSysId(2L);
+		mockEvent2.setDate(LocalDate.of(2024, 7, 23));
+		mockEvent2.setAvailableSpots(5);
 
 		reservation1 = new Reservation(LocalDate.of(2024, 1, 1), DayType.FULL_DAY, mockKid);
 		reservation1.setSysId(1L);
@@ -135,6 +147,77 @@ public class ReservationServiceUnitTest {
 		assertThat(responseDtoList.get(1).getDayType()).isEqualTo(reservation2.getDayType());
 
 		verify(reservationRepository, times(1)).findByKidSysId(anyLong());
+	}
+
+	@Test
+	void createReservation_withExistingReservation_throwsDuplicateReservationException() {
+		Reservation existingReservation = new Reservation(LocalDate.of(2024, 1, 22), DayType.FULL_DAY, mockKid);
+		existingReservation.setSysId(6L);
+
+		when(reservationRepository.findByKidIdAndDate(anyLong(), any())).thenReturn(Optional.of(reservation1));
+		when(kidRepository.findByUserName(anyString())).thenReturn(Optional.of(mockKid));
+
+		DuplicateReservationException exception = assertThrows(DuplicateReservationException.class, () -> {
+			reservationService.createReservation(creationRequestDto);
+		});
+
+		assertNotNull(exception);
+		assertEquals("Selected kid already has a reservation for this day", exception.getMessage());
+		assertEquals(HttpStatus.CONFLICT, exception.getStatus());
+	}
+
+	@Test
+	void createReservation_withAvailableSpotsOnDate_throwsGenericEventException() {
+		when(reservationRepository.findByKidIdAndDate(anyLong(), any())).thenReturn(Optional.empty());
+		when(kidRepository.findByUserName(anyString())).thenReturn(Optional.of(mockKid));
+		when(eventRepository.findByDate(any())).thenReturn(Optional.of(mockEvent2));
+
+		GenericEventException exception = assertThrows(GenericEventException.class, () -> {
+			reservationService.createReservation(creationRequestDto);
+		});
+
+		assertNotNull(exception);
+		assertEquals("There are available spots for the day, reservation was not created", exception.getMessage());
+		assertEquals(HttpStatus.CONFLICT, exception.getStatus());
+	}
+
+	@Test
+	void fetchReservationBySysId_withInvalidId_throwsGenericReservationException() {
+		when(reservationRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+		GenericReservationException exception = assertThrows(GenericReservationException.class, () -> {
+			reservationService.fetchReservationBySysId(anyLong());
+		});
+
+		assertNotNull(exception);
+		assertEquals("Reservation with this id was not found in the database.", exception.getMessage());
+		assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+	}
+
+	@Test
+	void fetchAllReservations_whenNoReservationExists_throwsGenericReservationException() {
+		when(reservationRepository.findAll()).thenReturn(new ArrayList<>());
+
+		GenericReservationException exception = assertThrows(GenericReservationException.class, () -> {
+			reservationService.fetchAllReservations();
+		});
+
+		assertNotNull(exception);
+		assertEquals("There are no reservations in the database", exception.getMessage());
+		assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+	}
+
+	@Test
+	void fetchReservationsByKidSysId_withNoReservationForKid_throwsGenericReservationException() {
+		when(reservationRepository.findByKidSysId(anyLong())).thenReturn(Optional.empty());
+
+		GenericReservationException exception = assertThrows(GenericReservationException.class, () -> {
+			reservationService.fetchReservationsByKidSysId(mockKid.getSysId());
+		});
+
+		assertNotNull(exception);
+		assertEquals("There are no reservations for kid with id 3 in the database", exception.getMessage());
+		assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
 	}
 
 }
