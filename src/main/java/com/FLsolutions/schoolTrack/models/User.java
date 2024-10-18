d07b3092334347d620b83a15022f01304664777e
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-10-18 22:13:31 +0200

    Implement UserDetails interface in User class to integrate with Spring Security

--- src/main/java/com/FLsolutions/schoolTrack/models/User.java
+++ src/main/java/com/FLsolutions/schoolTrack/models/User.java
@@ -1,8 +1,13 @@
 package com.FLsolutions.schoolTrack.models;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.FLsolutions.schoolTrack.services.Utils;
 
@@ -11,7 +16,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class User {
+public class User implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.TABLE)
@@ -25,12 +30,18 @@ public class User {
 	@Column(name = "user_name", unique = true, nullable = false)
 	private String userName;
 	private String password;
-	
+
+	@Enumerated(EnumType.STRING)
 	private Role role;
 
 	@CreationTimestamp
 	@Column(name = "created_at", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
 
 	public User() {
 	}
@@ -39,7 +50,7 @@ public class User {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.userName = Utils.createUserName(firstName, lastName);
-		this.role= Role.ROLE_USER;
+		this.role = Role.ROLE_USER;
 	}
 
 	public User(String firstName, String lastName, String email) {
@@ -49,13 +60,13 @@ public class User {
 		this.userName = Utils.createUserName(firstName, lastName);
 		// method to create safe password to be done
 		this.password = createPassword();
-		this.role= Role.ROLE_USER;
+		this.role = Role.ROLE_USER;
 	}
 
 	public User(String firstName, String lastName, String telNumber, String email) {
 		this(firstName, lastName, email);
 		this.telNumber = telNumber;
-		this.role= Role.ROLE_USER;
+		this.role = Role.ROLE_USER;
 	}
 
 //	public String createUserName(String firstName, String lastName) {
@@ -84,10 +95,6 @@ public class User {
 
 	public String getEmail() {
 		return email;
-	}
-
-	public String getUserName() {
-		return userName;
 	}
 
 	public void setFirstName(String firstName) {
@@ -137,5 +144,29 @@ public class User {
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public String getUsername() {
+		return userName;
+	}
 }
