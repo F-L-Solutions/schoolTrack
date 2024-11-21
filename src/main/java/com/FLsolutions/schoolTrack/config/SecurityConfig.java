fbea7fc078eff32b5dedca5abe43d4cf493bcefb
Frederika Horňáková <frederika.hornakova@gmail.com>
2024-11-21 13:57:52 +0100

    Added config for authentication

---
+++ src/main/java/com/FLsolutions/schoolTrack/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.FLsolutions.schoolTrack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.FLsolutions.schoolTrack.services.UserService;
+import com.FLsolutions.schoolTrack.filters.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
+
+	public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, UserService userService,
+			PasswordEncoder passwordEncoder) {
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Bean
+	AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userService.userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder);
+		return authProvider;
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				// Disable CSRF for stateless JWT-based authentication
+				.csrf(csrf -> csrf.disable())
+
+				// Set session management to stateless
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+				// Define request authorization rules
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers("/auth/**").permitAll()
+						.requestMatchers(HttpMethod.POST, "/admins").hasRole("SUPER_ADMIN")
+						.requestMatchers(HttpMethod.GET, "/test/**").permitAll()
+						.anyRequest().authenticated())
+				
+				// Set custom authentication provider
+				.authenticationProvider(authenticationProvider())
+				
+				// Add JWT filter before the UsernamePasswordAuthenticationFilter
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+}
---
+++ src/main/java/com/FLsolutions/schoolTrack/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,79 @@
+package com.FLsolutions.schoolTrack.filters;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.FLsolutions.schoolTrack.services.JwtService;
+import com.FLsolutions.schoolTrack.services.UserService;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+
+import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	private final JwtService jwtService;
+	private final UserService userService;
+
+	public JwtAuthenticationFilter(JwtService jwtService, UserService userService) {
+		this.jwtService = jwtService;
+		this.userService = userService;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		final String userEmail;
+
+		try {
+			if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+				filterChain.doFilter(request, response);
+				return;
+			}
+
+			jwt = authHeader.substring(7);
+			userEmail = jwtService.extractUserName(jwt);
+
+			if (StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+				UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+
+				if (jwtService.isTokenValid(jwt, userDetails)) {
+
+					SecurityContext context = SecurityContextHolder.createEmptyContext();
+					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+							null, userDetails.getAuthorities());
+
+					authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					context.setAuthentication(authToken);
+					SecurityContextHolder.setContext(context);
+				}
+			}
+			filterChain.doFilter(request, response);
+
+		} catch (IllegalArgumentException ex) {
+	        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+	        response.setContentType("application/json");
+	        response.getWriter().write("{ \"error\": \"" + ex.getMessage() + "\" }");
+	        return;
+		}
+	}
+}
